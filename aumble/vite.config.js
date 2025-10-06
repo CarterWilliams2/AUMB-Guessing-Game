2cc7f60ce39df1e3acaa242cc885acca924ce4da
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2025-10-06 12:51:37 -0500

    add test variables to conifg

--- aumble/vite.config.js
+++ aumble/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AUMB-Guessing-Game/'
+  base: '/AUMB-Guessing-Game/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
 })
