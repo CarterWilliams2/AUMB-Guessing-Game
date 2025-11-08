398a74212dafccc2adcd4075bacb9294888c4c25
Carter Williams <135449037+CarterWilliams2@users.noreply.github.com>
2025-11-07 21:41:58 -0600

    do alex black a favor

--- src/utils/getDailyMember.ts
+++ src/utils/getDailyMember.ts
@@ -16,8 +16,8 @@ export function getDailyMember(members: Guess[]): Guess {
   const day = parts.find(p => p.type === "day")?.value;
   const dateString = `${year}-${month}-${day}`;
 
-  if (dateString == '2025-10-15') {
-    const special = members.find(m => m.name == "Sam Thomas");
+  if (dateString == '2025-11-08') {
+    const special = members.find(m => m.name == "Tae Moon");
     if (!special) {
       return {
         auburnID: "",
